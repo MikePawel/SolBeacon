2b7c365e5854cdd2bef4c6355673d473f83850ee
MikePawel <radlinski.mikolaj@gmail.com>
2025-05-03 23:00:14 +0200

    added buffer and polyfils

--- Solana-frontend/vite.config.ts
+++ Solana-frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,25 +18,32 @@ export default defineConfig({
     esbuildOptions: {
       target: "esnext",
     },
+    include: ["buffer", "process"],
   },
   resolve: {
     alias: {
-      crypto: "empty-module",
-      assert: "empty-module",
-      http: "empty-module",
-      https: "empty-module",
-      os: "empty-module",
-      url: "empty-module",
-      zlib: "empty-module",
-      stream: "empty-module",
-      _stream_duplex: "empty-module",
-      _stream_passthrough: "empty-module",
-      _stream_readable: "empty-module",
-      _stream_writable: "empty-module",
-      _stream_transform: "empty-module",
+      buffer: "buffer",
+      process: "process",
+      stream: "stream-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      os: "os-browserify",
+      url: "url",
+      zlib: "zlib-browserify",
+      assert: "assert",
+      crypto: "crypto-browserify",
     },
   },
   define: {
     global: "globalThis",
+    "process.env": {},
+  },
+  build: {
+    rollupOptions: {
+      plugins: [],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 });
