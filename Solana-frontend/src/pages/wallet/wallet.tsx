d0805a6ba8b547ef282826311b29a336d89baefd
MikePawel <radlinski.mikolaj@gmail.com>
2025-05-06 15:39:56 +0200

    added nixpacks

--- Solana-frontend/src/pages/wallet/wallet.tsx
+++ Solana-frontend/src/pages/wallet/wallet.tsx
@@ -18,6 +18,7 @@ import "./wallet.css";
 const clientId =
   "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
 //Free one: BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ
+// from dashboard: BO64YmrgP9IlUvt2__kcdilBAdRuC7Q7-uGOajbW5UNpN69GLTFJmCFF1E0X-V2NLs5OM7VapVZuWCF-KTNT5kg
 
 export default function Wallet() {
   const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,10 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 const userRouter = require("./routes/user");
 app.use("/users", userRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-  console.log("API Documentation available at http://localhost:3000/api-docs");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(
+    `API Documentation available at http://localhost:${PORT}/api-docs`
+  );
 });
