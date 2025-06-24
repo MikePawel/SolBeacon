2732b779f010f0c3d73aceabf3aed9e657476989
MikePawel <radlinski.mikolaj@gmail.com>
2025-06-24 16:03:18 +0200

    simple esp32 code

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(921600);
+  Serial.println("Hello from setup");
+  pinMode(LED_BUILTIN, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
+  Serial.println("Hello from loop");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
